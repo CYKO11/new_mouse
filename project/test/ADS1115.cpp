fd0193013ca603d2504ce3b70eba851bfc0f4869
cyko <lmk310500@gmail.com>
2021-03-11 14:58:02 +0200

    added missing header

--- project/test/ADS1115.cpp
+++ project/test/ADS1115.cpp
@@ -2,6 +2,7 @@
 #include <fcntl.h>				//Needed for I2C port
 #include <sys/ioctl.h>			//Needed for I2C port
 #include <linux/i2c-dev.h>		//Needed for I2C port
+#include <iostream>
 
 using namespace std;
 
